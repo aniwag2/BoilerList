e21857a8155955555397f603f14dc1f50930f93e
aniwag2 <aniwaghray@gmail.com>
2025-06-16 17:34:31 -0400

    Implemented test api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// functions
+import { getTest }  from "./functions/test";
 
 function App() {
+  const [data, setData] = useState("Hello World");
+
+  useEffect(() => {
+    getTest()
+      .then((res) => {
+        setData(res.message);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>{data}</h1>
     </div>
   );
 }
